86d5e3b2874a77d7cac99ec771ffb5c2bcf062bc
Marty Sweet <msweet2214@gmail.com>
2016-12-12 07:11:01 +0000

    Add modified ported versions of alexa and rpi scripts

---
+++ alexa/functions/xmas/main.js
@@ -0,0 +1,200 @@
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+var AWS = require("aws-sdk");
+var iotdata = new AWS.IotData({endpoint: 'data.iot.eu-west-1.amazonaws.com'});
+var thingName = 'XMAS';
+var currentDesired;
+const UPPER_MODE_LIMIT = 7;
+
+/**
+ * Setup child
+ */
+var Xmas = function (callback) {
+    getThingShadow(callback);
+};
+
+/**
+ * Gets the thing shadow for the device
+ * @param callback
+ */
+function getThingShadow(callback){
+    var params = {
+        "thingName": thingName /* required */
+    };
+    iotdata.getThingShadow(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     {
+            console.log(data);
+            currentDesired = JSON.parse(data['payload'])['state']['desired'];
+            AlexaSkill.call(this, APP_ID);
+            callback();
+        }
+    });
+}
+
+// Extend AlexaSkill
+Xmas.prototype = Object.create(AlexaSkill.prototype);
+Xmas.prototype.constructor = Xmas;
+
+Xmas.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    //console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+Xmas.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    //console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    //handleNewFactRequest(response);
+};
+
+Xmas.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    //console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+/**
+ * Intent Handler
+ */
+Xmas.prototype.intentHandlers = {
+    "TurnDeviceOn": function (intent, session, response) {
+        changeDevicePowerState(response, 'ON');
+    },
+
+    "TurnDeviceOff": function (intent, session, response) {
+        changeDevicePowerState(response, 'OFF');
+    },
+
+    "ChangeMode": function (intent, session, response) {
+        changeMode(response, intent);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("You can say...");
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Turn the lights on or off
+ * @param response
+ * @param state
+ */
+function changeDevicePowerState(response, state){
+    var message = 'Turning Xmas ' + state;
+    var callback;
+    var value;
+    if(state == 'ON') {
+        callback = function(){response.ask(message);};
+        value = 1;
+    }else{
+        callback = function(){response.tell(message);};
+        value = 0;
+    }
+    // Send message
+    updateIotShadow({"power": value, "mode": 0}, callback);
+}
+
+/**
+ * Set the mode depending on the users SlotType values.
+ * @param response
+ * @param intent
+ */
+function changeMode(response, intent){
+    var desired = intent['slots']['LightMode']['value'];
+    var value = 0;
+    switch(desired){
+        case 'slowly':
+            value = 0;
+            break;
+        case 'next':
+            value = increaseMode();
+            break;
+        case 'last':
+            value = decreaseMode();
+            break;
+        default:
+            console.log("Unhandled Mode Request: " + desired);
+            break;
+    }
+
+    updateIotShadow({"power": 1, "mode": value}, function(){successfulChange(response)});
+
+}
+
+/**
+ * Decrease the mode from the current mode value
+ */
+function decreaseMode(){
+    var x = currentDesired["mode"] - 1;
+    return getModeInRange(x);
+}
+
+/**
+ * Increase the mode from the current mode value
+ */
+function increaseMode(){
+    var x = currentDesired["mode"] + 1;
+    return getModeInRange(x);
+}
+
+/**
+ * Ensure the mode is within the valid MODE range
+ * @param x
+ * @returns {*}
+ */
+function getModeInRange(x){
+    if(x >= 0 && x <= UPPER_MODE_LIMIT){
+        return x
+    }else{
+        // Maybe have nicer feedback if this is the case
+        return currentDesired["mode"];
+    }
+}
+
+/**
+ * If a change has been acknowledged, can be expanded
+ * to have an array of responses to add variety.
+ */
+function successfulChange(response){
+    response.ask('Operation complete...');
+}
+
+/**
+ * Send a message to Xmas
+ */
+function updateIotShadow(json, callback){
+    var params = {
+        payload: JSON.stringify({"state": { "desired": json} }), /* required */
+        thingName: thingName /* required */
+    };
+    iotdata.updateThingShadow(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     callback();          // successful response
+    });
+
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the SpaceGeek skill.
+    var bob = new Xmas(function(){
+        // Calls context succeed
+        bob.execute(event, context);
+    });
+};
+
---
+++ rpi/client.js
@@ -0,0 +1,306 @@
+var shadow;
+
+/** Includes **/
+var gpio = require('rpi-gpio');
+var async = require('async');
+var awsIot = require('aws-iot-device-sdk');
+
+var thingName = 'XMAS';
+
+/** GPIO Switch (for development) **/
+var GPIOEnabled = false;
+
+/** Power **/
+var pCurrent = 0;
+var PIN_POWER = 11;
+
+/** Mode Changer (Assumes on power resume, the mode is always the first) **/
+var mCurrent = 0;
+var mUpperBound = 7;
+var PIN_MODE = 13;
+
+/** Pulse time must be long enough for the relay to engage and the controller to recognise the switch press. **/
+var PULSE_TIME_MS = 50;
+
+/** Setup pins **/
+if(GPIOEnabled) {
+    gpio.setup(PIN_POWER, function(err){
+        console.log("Power pin setup. " + err);
+    });
+    gpio.setup(PIN_MODE, function(err){
+        console.log("Mode pin setup. " + err);
+    });
+}
+
+/**
+ * AWS Handling of events
+ */
+function connectToAWS(){
+
+    shadow = awsIot.thingShadow({
+        "host": "data.iot.eu-west-1.amazonaws.com",
+        "port": 8883,
+        "clientId": thingName,
+        "thingName": thingName,
+        "caCert": "root-CA.crt",
+        "clientCert": "certificate.pem.crt",
+        "privateKey": "private.pem.key",
+        "region": "eu-west-1"
+    });
+
+    shadow.on('connect', function() {
+        console.log("AWS: connected");
+        setTimeout(function(){
+            shadow.register(thingName, enableVersioning=true,
+                function() {
+                    console.log("Getting latest shadow");
+                    shadow.get(thingName);
+                });
+        }, 500);
+    });
+
+    shadow.on('delta', function(thingName, stateObject) {
+        //console.log('received delta on '+thingName+': ' + JSON.stringify(stateObject));
+        // Pass the state message for handling
+        handleMessage(stateObject['state']);
+    });
+
+    shadow.on('status',
+        function(thingName, stat, clientToken, stateObject) {
+            // console.log('received '+stat+' on '+thingName+': ' + JSON.stringify(stateObject));
+            if(stateObject["state"]["desired"]){
+                handleMessage(stateObject['state']);
+            }
+        });
+
+    shadow.on('error', function(error) {
+        console.log("AWS: Error - " + error);
+    });
+
+    shadow.on('reconnect', function() {
+        console.log("AWS: Error - Reconnecting");
+    });
+}
+
+/**
+ * Takes the message and directs it to the specific handler.
+ * This is done is series to ensure the last action has applied before the next
+ * one is attempted. Otherwise, for example, the power may be processed but not outputted
+ * for the mode is attempted to be switched.
+ * @param object
+ */
+function handleMessage(object){
+    async.series([
+        function(callback){
+            if(typeof object['power'] !== 'undefined'){
+                setPower(object['power'], callback);
+            }else{
+                callback();
+            }
+        },
+        function(callback){
+            if(typeof object['mode'] !== 'undefined'){
+                setMode(object['mode'], callback);
+            }else{
+                callback();
+            }
+        }
+    ], function (err, result){
+        console.log("Done");
+        updateShadow();
+    });
+}
+
+/**
+ * Updates the shadow with the current settings (as we know them)
+ */
+function updateShadow(){
+    // Ensure we output INT, not strings
+    var state = {"power": parseInt(pCurrent), "mode": parseInt(mCurrent)};
+    shadow.update(thingName, {"state": {"reported": state}});
+}
+
+/// POWER ///
+/**
+ * Set the power to the correct value
+ * @param val
+ */
+function setPower(val, gpioDoneCallback){
+    if (val == 0) {
+        ensureDeviceOff(gpioDoneCallback);
+    }else{
+        ensureDeviceOn(gpioDoneCallback);
+    }
+}
+
+/**
+ * Ensure the device is turned on
+ */
+function ensureDeviceOn(gpioDoneCallback){
+    if(pCurrent == 0){
+        pulseGPIOPin(PIN_POWER, 1, function(){
+            pCurrent = 1;
+            gpioDoneCallback();
+        });
+    }else{
+        gpioDoneCallback()
+    }
+}
+
+/**
+ * Ensure the device is turned off, for the mode to 0 (as this is what will happen).
+ * Assume the Alexa Skill will do the same otherwise the Shadows will become out of sync.
+ */
+function ensureDeviceOff(gpioDoneCallback){
+    if(pCurrent == 1){
+        pulseGPIOPin(PIN_POWER, 1, function(){
+            pCurrent = 0;
+            // Update the values as the device is now off
+            mCurrent = 0;
+            gpioDoneCallback();
+        });
+    }else{
+        gpioDoneCallback();
+    }
+}
+
+/// MODE ///
+
+/**
+ * Set the mode of the device to the desired input value
+ * @param inputValue
+ * @param gpioDoneCallback
+ */
+function setMode(inputValue, gpioDoneCallback){
+    if(0 <= inputValue && mUpperBound >= inputValue){
+        changePhysicalPushSwitchValue(PIN_MODE, mCurrent, mUpperBound, inputValue, function(){
+            mCurrent = inputValue;
+            gpioDoneCallback();
+        });
+    }else{
+        console.log("Mode: Invalid input value: " + inputValue);
+        gpioDoneCallback();
+    }
+}
+
+/// GPIO ///
+
+/**
+ * Change value depending on the input parameters.
+ * @param pin
+ * @param current
+ * @param upperBound
+ * @param desired
+ * @param successCallback
+ */
+function changePhysicalPushSwitchValue(pin, current, upperBound, desired, successCallback){
+    if(current == desired){
+        // No need to change
+        console.log("No need to change for pin: " + pin);
+        successCallback();
+    }else if(desired == 0) {
+        // Need a long pause to turn the shadow off
+        // TODO: We shouldn't need a long pause with this design
+        pulseGPIOPin(pin, 1, successCallback);
+    }else if(desired > current){
+        // We can do a direct pulse, no need to loop
+       // ensureDeviceOn();
+        pulseGPIOPin(pin, desired - current, successCallback);
+    }else{
+        // Need to loop using upperBound
+        //ensureDeviceOn();
+        pulseGPIOPin(pin, ((upperBound - current) + desired), successCallback);
+    }
+}
+
+/**
+ * Pulse the GPIO Pin with a statically defined interval, in order to simulate
+ * switch presses on the shadow.
+ * @param pin
+ * @param count
+ * @param callback
+ */
+function pulseGPIOPin(pin, count, callback){
+    console.log("Pulsing for pin " + pin + " for count: " + count);
+    outputOnGPIOCountDuration(pin, PULSE_TIME_MS, count, callback);
+}
+
+/**
+ * Manages the pin calling in an asynchronous fashion, calling successCallback
+ * once remainingCount == 0.
+ * @param pin
+ * @param time
+ * @param remainingCount
+ * @param successCallback
+ */
+function outputOnGPIOCountDuration(pin, time, remainingCount, successCallback){
+    async.series([
+        function (callback){
+            console.log(remainingCount + " pulses remaining for pin " + pin);
+            outputOnGPIODuration(pin, PULSE_TIME_MS, callback);
+        }
+    ], function (err, result){
+        if((remainingCount - 1 ) > 0) {
+            outputOnGPIOCountDuration(pin, time, remainingCount - 1, successCallback);
+        }else{
+            successCallback();
+            outputDeviceSummary();
+        }
+    })
+}
+
+/**
+ * Handles on ON pause OFF sequence
+ * @param pin
+ * @param duration
+ * @param finishedCallback
+ */
+function outputOnGPIODuration(pin, duration, finishedCallback) {
+    console.log("Outputting pin " + pin + " for " + duration + " ms");
+    async.series([
+            function (callback) {
+                writeGPIO(pin, true, callback);
+            },
+            function (callback) {
+                setTimeout(function () {
+                    writeGPIO(pin, false, callback);
+                }, duration);
+            }
+        ],
+        function (err, result) {
+            console.log("On/off cycle complete");
+            finishedCallback()
+        });
+}
+
+/**
+ * Wrapper function around write to make debugging and further
+ * improvements easier.
+ * @param pin
+ * @param value
+ * @param callback
+ */
+function writeGPIO(pin, value, callback) {
+    console.log("Outputting " + value + " to pin " + pin);
+    if(GPIOEnabled) {
+        gpio.write(pin, value, callback);
+    }else{
+        callback();
+    }
+}
+
+/// HELPERS ///
+
+/**
+ * Output a summary of the shadow state to the console.
+ */
+function outputDeviceSummary(){
+    console.log("============================");
+    console.log("Power: " + pCurrent);
+    console.log("Mode: " + mCurrent);
+    console.log("============================");
+}
+
+
+// Starting function
+connectToAWS();
